ccfd20432e923ab8e56dcb3e2f33fa87a451c697
LoganLieou <logan.lieou@gmail.com>
2022-10-03 19:12:51 -0500

    second

--- bin/main.ml
+++ bin/main.ml
@@ -9,10 +9,13 @@ let getBody url =
   (* if Client.get fails don't run the function on the right *)
   Client.get (Uri.of_string url) >>= fun (_, body) -> body |> Cohttp_lwt.Body.to_string
 
-(* match https://www.gutenberg.org/ebooks/bookshel *)
-let process s q =
-  if String.starts_with ~prefix:"/ebooks/bookshel" s then
-    Queue.push ("https://www.gutenberg.org" ^ s) q
+(* process each page of titles *)
+let process s =
+  let res  = Queue.create () in
+  let soup = Lwt_main.run (getBody s) |> parse in
+  (* select all titles from the page and push to queue *)
+  soup $$ ".title" |> iter (fun t -> Queue.push (R.leaf_text t) res);
+  res
 
 let () =
   (* get webpage from url *)
@@ -21,9 +24,18 @@ let () =
 
   (* lambdasoup basic parse *)
   let links = Queue.create () in
-  let genres = Queue.create () in
   let soup = body |> parse in
-  soup $$ "a[title]" |> iter (fun a -> Queue.push (R.attribute "title" a) genres);
-  soup $$ "a[href]" |> iter (fun a -> process (R.attribute "href" a) links);
-  Queue.iter (print_endline) genres;
-  
+
+  (* get list of all next links to crawl *)
+  soup $$ "a[href]" |> iter (fun a -> 
+    if String.starts_with ~prefix:"/ebooks/bookshel" (R.attribute "href" a) then
+      Queue.push ("https://www.gutenberg.org" ^ (R.attribute "href" a)) links);
+
+  (* for each title crawl that link for all titles belonging to that genre *)
+  soup $$ "a[title]" |> iter (fun a ->
+    (* title is genre -> list of book titles *)
+    Hashtbl.add data (R.attribute "title" a) (process (Queue.take links)));
+
+  (* output the resultant data table *)
+  Hashtbl.iter (fun k v -> print_endline k; print_endline (Queue.take v)) data
+;;
